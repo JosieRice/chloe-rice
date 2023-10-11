67dcccd89247ee885c1fa253dc6453c5cc89eb2b
JosieRice <josiericetong@gmail.com>
2023-10-11 14:49:49 -0700

    added a hero banner for the site

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,5 +3,8 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/components/**/*.{ts,tsx}", "./src/app/**/*.{ts,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["forest"],
+  },
 };
 export default config;
