cc7ea1a61bb8e004f64c3a53d9b1c626eafa0504
JosieRice <josiericetong@gmail.com>
2023-10-12 17:25:54 -0700

    added tooltips to languages and skills

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -5,18 +5,22 @@ export function Languages() {
     <div className="flex flex-col mt-8">
       <h1 className="text-5xl font-bold text-center">Languages</h1>
       <div className="flex flex-wrap gap-8 justify-center mt-8">
-        <Image
-          alt="javascript"
-          src={"/depIcons/javascript.png"}
-          width={80}
-          height={80}
-        />
-        <Image
-          alt="typescript"
-          src={"/depIcons/typescript.png"}
-          width={80}
-          height={80}
-        />
+        <div className="tooltip" data-tip="JavaScript">
+          <Image
+            alt="javascript"
+            src={"/depIcons/javascript.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="TypeScript">
+          <Image
+            alt="typescript"
+            src={"/depIcons/typescript.png"}
+            width={80}
+            height={80}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,64 +6,120 @@ export function Skills() {
       <h1 className="text-5xl font-bold text-center">Skills</h1>
       <div className="flex flex-wrap gap-8 justify-center mt-8 mb-16">
         {/* most images from https://www.stickpng.com/ */}
-        <Image alt="react" src={"/depIcons/react.png"} width={80} height={80} />
-        <Image alt="HTML5" src={"/depIcons/html.png"} width={80} height={80} />
-        <Image alt="CSS3" src={"/depIcons/css.png"} width={80} height={80} />
-        <Image
-          alt="graphQL"
-          src={"/depIcons/graphql.png"}
-          width={80}
-          height={80}
-        />
-        <Image
-          alt="Next Js"
-          src={"/depIcons/nextjs.png"}
-          width={80}
-          height={80}
-        />
-        <Image
-          alt="react native"
-          src={"/depIcons/react-native.png"}
-          width={80}
-          height={80}
-        />
-        <Image
-          alt="nodejs"
-          src={"/depIcons/nodejs.png"}
-          width={80}
-          height={80}
-        />
-        <Image alt="npm" src={"/depIcons/npm.png"} width={80} height={80} />
-        <Image
-          alt="webpack"
-          src={"/depIcons/webpack.png"}
-          width={80}
-          height={80}
-        />
-        <Image alt="babel" src={"/depIcons/babel.png"} width={80} height={80} />
-        <Image alt="jest" src={"/depIcons/jest.png"} width={80} height={80} />
-        <Image
-          alt="backbone"
-          src={"/depIcons/backbone.png"}
-          width={80}
-          height={80}
-        />
-        <Image alt="redux" src={"/depIcons/redux.png"} width={80} height={80} />
-        <Image alt="mui" src={"/depIcons/mui.png"} width={80} height={80} />
-        <Image
-          alt="google maps api"
-          src={"/depIcons/googlemapsapi.png"}
-          width={80}
-          height={80}
-        />
-        <Image alt="less" src={"/depIcons/less.png"} width={80} height={80} />
-        <Image alt="git" src={"/depIcons/git.png"} width={80} height={80} />
-        <Image
-          alt="github"
-          src={"/depIcons/github.png"}
-          width={80}
-          height={80}
-        />
+        <div className="tooltip" data-tip="React">
+          <Image
+            alt="react"
+            src={"/depIcons/react.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="HTML5">
+          <Image
+            alt="HTML5"
+            src={"/depIcons/html.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="CSS3">
+          <Image alt="CSS3" src={"/depIcons/css.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="graphQL">
+          <Image
+            alt="graphQL"
+            src={"/depIcons/graphql.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="Next.js">
+          <Image
+            alt="Next Js"
+            src={"/depIcons/nextjs.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="React Native">
+          <Image
+            alt="react native"
+            src={"/depIcons/react-native.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="Node.js">
+          <Image
+            alt="nodejs"
+            src={"/depIcons/nodejs.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="npm">
+          <Image alt="npm" src={"/depIcons/npm.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="webpack">
+          <Image
+            alt="webpack"
+            src={"/depIcons/webpack.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="babel">
+          <Image
+            alt="babel"
+            src={"/depIcons/babel.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="jest">
+          <Image alt="jest" src={"/depIcons/jest.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="backbone">
+          <Image
+            alt="backbone"
+            src={"/depIcons/backbone.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="redux">
+          <Image
+            alt="redux"
+            src={"/depIcons/redux.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="mui">
+          <Image alt="mui" src={"/depIcons/mui.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="Google Maps API">
+          <Image
+            alt="google maps api"
+            src={"/depIcons/googlemapsapi.png"}
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="tooltip" data-tip="LESS preprocesor">
+          <Image alt="less" src={"/depIcons/less.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="git">
+          <Image alt="git" src={"/depIcons/git.png"} width={80} height={80} />
+        </div>
+        <div className="tooltip" data-tip="GitHub">
+          <Image
+            alt="github"
+            src={"/depIcons/github.png"}
+            width={80}
+            height={80}
+          />
+        </div>
       </div>
     </div>
   );
