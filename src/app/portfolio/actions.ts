c784da5983d8e2940961c0719a7c886ae017fd64
JosieRice <josiericetong@gmail.com>
2023-10-16 13:41:20 -0700

    added a portfolio section with mvp of recent github repos

---
+++ src/app/portfolio/actions.ts
@@ -0,0 +1,15 @@
+"use server";
+
+export async function getGitHubRepos() {
+  const res = await fetch(
+    "https://api.github.com/users/josierice/repos?sort=updated&order=desc&page=1&per_page=6",
+    { next: { revalidate: 3600 } }
+  );
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch github repos");
+  }
+
+  return res.json();
+}
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,28 @@
-export default function Portfolio() {
+import { GitHubCard } from "@/components/GitHubCard";
+import { getGitHubRepos } from "./actions";
+
+export default async function Portfolio() {
+  const repos = await getGitHubRepos();
+
   return (
-    <h1 className="text-xl font-bold text-center mt-16">
-      Coming soon, some porfolio pieces.
-    </h1>
+    <div className="container mx-auto px-4">
+      <h1 className="text-5xl font-bold text-center">Portfolio</h1>
+      <h2 className="text-xl font-bold text-center mt-8 mb-8">
+        GitHub at a glance
+      </h2>
+      <div className="flex flex-wrap gap-8 justify-center">
+        {repos?.map((repo: any, i: number) => (
+          <GitHubCard
+            key={repo.id}
+            createdAt={repo.created_at}
+            description={repo.description}
+            htmlUrl={repo.html_url}
+            language={repo.language}
+            name={repo.name}
+            privateBool={repo.private}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
