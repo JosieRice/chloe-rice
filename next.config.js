d093d61c85e708b6df8c30b5c7940e66a7a77581
JosieRice <josiericetong@gmail.com>
2023-10-19 14:07:35 -0700

    added auto formatting for .js files for config files that cant be .ts

--- next.config.js
+++ next.config.js
@@ -5,21 +5,21 @@ const nextConfig = {
     },
     async headers() {
         return [
-          {
-            headers: [
-              {
-                key: 'X-Frame-Options',
-                value: 'SAMEORIGIN',
-              },
-              {
-                key: 'Content-Security-Policy',
-                value: `frame-ancestors 'self' https://app.contentful.com`,
-              },
-            ],
-            source: '/:path*',
-          },
+            {
+                headers: [
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: `frame-ancestors 'self' https://app.contentful.com`,
+                    },
+                ],
+                source: "/:path*",
+            },
         ];
-      },
+    },
 };
 
 module.exports = nextConfig;
