a3e7c7bfe4db8cda06908ee6bba400b8d719008a
JosieRice <josiericetong@gmail.com>
2023-10-19 10:55:56 -0700

    updated html to section for each skill section

--- src/components/SkillSection.tsx
+++ src/components/SkillSection.tsx
@@ -6,9 +6,9 @@ interface Props extends PropsWithChildren {
 
 export function SkillSection({ children, title }: Props) {
     return (
-        <div className="flex flex-col mt-8">
+        <section className="flex flex-col mt-8">
             <h1 className="text-5xl font-bold text-center">{title}</h1>
             <div className="flex flex-wrap gap-8 justify-center mt-8 mb-8">{children}</div>
-        </div>
+        </section>
     );
 }
