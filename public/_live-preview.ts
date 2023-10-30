a17418f1ed2b2a2274e3dfbbbb5e0ddf743612a5
JosieRice <josiericetong@gmail.com>
2023-10-30 13:12:31 -0700

    added queries for images in blog posts

--- public/_live-preview.ts
+++
@@ -1,8 +0,0 @@
-import { ContentfulLivePreview } from "@contentful/live-preview";
-
-ContentfulLivePreview.init({ debugMode: true, enableLiveUpdates: true, locale: "en-US" });
-ContentfulLivePreview.subscribe("save", {
-    callback: () => {
-        window.location.reload();
-    },
-});
